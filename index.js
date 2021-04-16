57d9248388d025a3cdbef5366cd3802efc2a6b26
Michael Kim <mikesungunkim@gmail.com>
2021-04-16 22:23:19 +0900

    add empty Navigation

--- index.js
+++ index.js
@@ -2,8 +2,23 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {Navigation} from 'react-native-navigation';
 import {name as appName} from './app.json';
 import App from './src/App.tsx';
 
-AppRegistry.registerComponent(appName, () => App);
+Navigation.registerComponent(`com.${appName}.RootScreen`, () => App);
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      stack: {
+        children: [
+          {
+            component: {
+              name: `com.${appName}.RootScreen`,
+            },
+          },
+        ],
+      },
+    },
+  });
+});
