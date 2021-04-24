86ceec721a3cde4f12c81a2e1c38aee0f1285066
Michael Kim <mikesungunkim@gmail.com>
2021-04-24 18:20:59 +0900

    refactor login config code

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
  */
 
 import {registerScreens, registerRoot} from './src/navigation';
+import {configureGoogleLogin} from './src/services/googleSigninConfig.tsx';
 
 registerScreens();
 registerRoot();
+
+configureGoogleLogin();
--- src/components/GoogleLogin.tsx
+++ src/components/GoogleLogin.tsx
@@ -1,28 +1,59 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-google-signin/google-signin';
 
 interface Props {
   name?: string;
 }
 interface State {
   value: number;
+  isSigninInProgress: boolean;
 }
 class GoogleLogin extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = {
       value: 7,
+      isSigninInProgress: false,
     };
   }
 
+  signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      //   this.setState({userInfo});
+      console.log(userInfo.user);
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (e.g. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+        // some other error happened
+      }
+    }
+  };
+
   render(): JSX.Element {
     const {name} = this.props;
-    const {value} = this.state;
+    const {value, isSigninInProgress} = this.state;
     return (
-      <View>
-        <Text>Hello {name} </Text>
-        <Text>Value {value} </Text>
-      </View>
+      <GoogleSigninButton
+        style={{width: 192, height: 48}}
+        size={GoogleSigninButton.Size.Wide}
+        color={GoogleSigninButton.Color.Dark}
+        onPress={() => {
+          this.signIn();
+          this.setState({isSigninInProgress: true});
+        }}
+        disabled={isSigninInProgress}
+      />
     );
   }
 }
