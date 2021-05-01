b1ff9ce4564bf9d430a8fe44286e2ed7c4d40198
mikesukim <mikesungunkim@gmail.com>
2021-05-02 02:14:02 +0900

    add redux persist

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,14 +1,27 @@
 import {combineReducers} from 'redux';
+import {persistReducer} from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 import counter from './counter.ts';
 import auth from './auth.ts';
 
-const rootReducer = combineReducers({
+const persistConfig = {
+  key: 'root',
+  // localStorage에 저장합니다.
+  storage: AsyncStorage,
+  // reducer들중에 auth reducer만 localstorage에 저장합니다.
+  whitelist: ['auth'],
+  // blacklist -> 그것만 제외합니다
+};
+
+const rootReducer = combineReducers({ 
   counter,
   auth,
 });
 
 // 루트 리듀서를 내보내주세요.
-export default rootReducer;
+// export default rootReducer;
+export default persistReducer(persistConfig, rootReducer);
 
 // 루트 리듀서의 반환값를 유추해줍니다
 // 추후 이 타입을 컨테이너 컴포넌트 / 후크에서 불러와서 사용해야 하므로 내보내줍니다.
