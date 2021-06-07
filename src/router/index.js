e1538621acddc2581fb475e5f2c5d1d6c99eae8b
mikesukim <mikesungunkim@gmail.com>
2021-06-07 00:12:09 -0500

    refactor router

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,15 @@
 import axios from 'axios';
+const BASEURL = 'https://rsmz180w6a.execute-api.ap-northeast-2.amazonaws.com';
+const STAGE = 'dev';
 
 export function getHelloWorld(token) {
   return new Promise((resolve, reject) => {
     axios
-      .get(
-        'https://rsmz180w6a.execute-api.ap-northeast-2.amazonaws.com/dev/hello',
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
+      .get(`${BASEURL}/${STAGE}/hello`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
         },
-      )
+      })
       .then(function (response) {
         // handle success
         resolve(response);
@@ -25,14 +24,11 @@ export function getHelloWorld(token) {
 export function postLogin(email) {
   return new Promise((resolve, reject) => {
     axios
-      .post(
-        'https://rsmz180w6a.execute-api.ap-northeast-2.amazonaws.com/dev/user/login',
-        {
-          email: email,
-          pass: '1234',
-          appkey: 'TiHousework_lala',
-        },
-      )
+      .post(`${BASEURL}/${STAGE}/user/login`, {
+        email: email,
+        pass: '1234',
+        appkey: 'TiHousework_lala',
+      })
       .then(function (response) {
         // handle success
         resolve(response);
