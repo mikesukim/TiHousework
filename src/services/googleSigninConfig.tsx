acc692e73e85aef52aa580efaf55ec76de0c8b33
mikesukim <mikesungunkim@gmail.com>
2021-06-07 14:50:51 -0500

    fix google login ios

--- src/services/googleSigninConfig.tsx
+++ src/services/googleSigninConfig.tsx
@@ -6,6 +6,7 @@ export function configureGoogleLogin(): void {
     androidClientId:
       '889842190734-e98h63o1un4jbic4qqj4jqjp4l4vta64.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
     iosClientId:
-      '889842190734-2teqqqu1ttfnt5aa3muiphndmh8b1cn8.apps.googleusercontent.com', // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+      '1037600109526-ubqj42har7qe8gmm1dta4mneerhk3okm.apps.googleusercontent.com', // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+      
   });
 }
