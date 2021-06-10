6a15e4eebface8544d029e4c83fe9c713e61ecb7
Jiyun Noh <ianjy1127@gmail.com>
2021-06-10 13:15:23 -0500

    Add dynamic links

--- src/screens/App.tsx
+++ src/screens/App.tsx
@@ -6,9 +6,44 @@ import rootReducer from '../redux';
 import DefaultReactScreen from './DefaultReactScreen.tsx';
 import SocialLogin from '../components/SocialLogin';
 
+import dynamicLinks from '@react-native-firebase/dynamic-links'; 
+import { Alert } from 'react-native';
+
 const store = createStore(rootReducer);
 
 function App(): JSX.Element {
+  const handleDynamicLink = link => {
+    // alert(JSON.stringify(link));
+    // alert(link.url);
+
+    if (link.url === 'https://google.com') {
+      alert(JSON.stringify('This is google page'));
+    }
+
+    if (link.url === 'https://naver.com') {
+      alert(JSON.stringify('This is naver page!'));
+    }
+
+    // // Handle dynamic link inside your own application
+    // if (link.url === 'https://invertase.io/offer') {
+    //   // ...navigate to your offers screen
+    // }
+  };
+
+  React.useEffect(() => {
+    const unsubscribe = dynamicLinks().onLink(handleDynamicLink);
+    // When the component is unmounted, remove the listener
+    return () => unsubscribe();
+
+    // dynamicLinks()
+    // .getInitialLink()
+    // .then(link => {
+    //   if (link.url === 'https://invertase.io/offer') {
+    //     // ...set initial route as offers screen
+    //   }
+    // });
+  }, []);
+
   return (
     <Provider store={store}>
       <SocialLogin />
