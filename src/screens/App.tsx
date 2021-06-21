b2eb40494f1dd612c89782f65d44d3b39a952ad1
Jiyun Noh <ianjy1127@gmail.com>
2021-06-21 14:37:15 -0500

    Edit useEffect

--- src/screens/App.tsx
+++ src/screens/App.tsx
@@ -2,52 +2,63 @@ import React, {useEffect} from 'react';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import SplashScreen from 'react-native-splash-screen';
+import dynamicLinks from '@react-native-firebase/dynamic-links';
 
 import {persistStore} from 'redux-persist';
 import {PersistGate} from 'redux-persist/integration/react';
 import rootReducer from '../redux';
-import DefaultReactScreen from './DefaultReactScreen';
+
 import SocialLogin from '../components/SocialLogin';
 import ApiTestComp from '../components/ApiTestComp';
-
-import dynamicLinks from '@react-native-firebase/dynamic-links';
-import {Alert} from 'react-native';
+import useAuth from '../hooks/useAuth';
+import useUser from '../hooks/useUser';
 
 const store = createStore(rootReducer);
 const persistor = persistStore(store);
 
 function App(): JSX.Element {
-  const handleDynamicLink = link => {
-    // alert(JSON.stringify(link));
-    // alert(link.url);
+  // const {isInvited, onUpdateIsInvited} = useUser();
+  const configDynamicLink = () => {
+    const handleDynamicLink = link => {
+      // onUpdateIsInvited(true);
+      // // Handle dynamic link inside your own application
+      // if (link.url === 'https://invertase.io/offer') {
+      //   // ...navigate to your offers screen
+      // }
+      if (link.url === 'https://google.com') {
+        alert(JSON.stringify('This is google page'));
+      }
+      if (link.url === 'https://naver.com') {
+        alert(JSON.stringify('This is naver page!'));
+      }
+    };
 
-    if (link.url === 'https://google.com') {
-      alert(JSON.stringify('This is google page'));
-    }
-
-    if (link.url === 'https://naver.com') {
-      alert(JSON.stringify('This is naver page!'));
-    }
-
-    // // Handle dynamic link inside your own application
-    // if (link.url === 'https://invertase.io/offer') {
-    //   // ...navigate to your offers screen
-    // }
-  };
-
-  useEffect(() => {
-    SplashScreen.hide();
     dynamicLinks()
       .getInitialLink()
       .then(link => {
-          alert(link.url);
+        // onUpdateIsInvited(true);
+        alert('네 링크타고 들어왔어요');
+        // 네 링크타고 들어왔어요
         // if (link.url === 'https://invertase.io/offer') {
         //   // ...set initial route as offers screen
         // }
       });
+
     const unsubscribe = dynamicLinks().onLink(handleDynamicLink);
     // When the component is unmounted, remove the listener
     return () => unsubscribe();
+  };
+
+  // const hasToken = props => {
+  //   const {token} = props.token;
+  //   if (!token) {
+  //     return <SocialLogin />;
+  //   }
+  // };
+
+  useEffect(() => {
+    SplashScreen.hide();
+    configDynamicLink();
   }, []);
 
   return (
