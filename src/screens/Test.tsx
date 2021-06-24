2c162666c1868f58162d201742387d2018c3eede
Jiyun Noh <ianjy1127@gmail.com>
2021-06-23 23:45:53 -0500

    Add postRegister

--- src/screens/Test.tsx
+++ src/screens/Test.tsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import {Text, View} from 'react-native';
 import RoomCheckToScreenMW from '../components/RoomCheckToScreenMW';
 import useMaintenance from '../hooks/useMaintenance';
+import InvitationTokenCheckMW from '../components/InvitationTokenCheckMW';
 import LoginRegisterMW from '../components/LoginRegisterMW';
 
 function Test(): JSX.Element {
@@ -9,7 +10,8 @@ function Test(): JSX.Element {
     <View>
       {/* <Text>src/screen 에 위치한 Test 컴포넌트에, 테스트하고싶은 컴포넌트를 추가해 테스트하세요</Text> */}
       <RoomCheckToScreenMW />
-      <LoginRegisterMW />
+      <InvitationTokenCheckMW />
+      {/* <LoginRegisterMW /> */}
     </View>
   );
 }
