71db56437264613f87c8351724b10479c06f82c5
mikesukim <mikesungunkim@gmail.com>
2021-07-08 16:06:40 -0500

    Update Test.tsx

--- src/screens/Test.tsx
+++ src/screens/Test.tsx
@@ -6,7 +6,8 @@ function Test(): JSX.Element {
   return (
     <View>
       {/* <Text>src/screen 에 위치한 Test 컴포넌트에, 테스트하고싶은 컴포넌트를 추가해 테스트하세요</Text> */}
-      <Websocket />
+      {/* <Websocket /> */}
+      <Text> hello world </Text>
     </View>
   );
 }
