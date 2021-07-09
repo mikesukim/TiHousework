125ce04f376e0121c179914827e8cc0ba04eef72
mikesukim <mikesungunkim@gmail.com>
2021-07-09 00:27:23 -0500

    refactor after merge

--- src/screens/Test.tsx
+++ src/screens/Test.tsx
@@ -1,18 +1,13 @@
-import React, {useEffect, useRef, useState} from 'react';
-import {Text, View} from 'react-native';
-import SocialLogin from '../components/SocialLogin';
-import Websocket from '../components/Websocket';
-import {Header1} from '../styles/StyledComps'
+import React from 'react';
+import {Header1} from '../styled-components/StyledComps'
 
 function Test(): JSX.Element {
-  const websocketMsgSender = useRef(null);
-  const websocket = useRef(null);
   return (
-    <Container>
+    <>
       {/* <Text>src/screen 에 위치한 Test 컴포넌트에, 테스트하고싶은 컴포넌트를 추가해 테스트하세요</Text> */}
       {/* <Websocket /> */}
       <Header1> hello world </Header1>
-    </View>
+    </>
   );
 }
 
