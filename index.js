342d495dab56d50edf774fdfe5bb5b5c80881211
mikesukim <mikesungunkim@gmail.com>
2021-07-08 19:00:31 -0500

    add react navigation

--- index.js
+++ index.js
@@ -2,18 +2,8 @@
  * @format
  */
 
-// import {registerScreens, registerRoot} from './src/navigation';
-
-
-// registerScreens();
-// registerRoot();
-
-
-
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 import App from './src/screens/App.tsx';
-import {configureGoogleLogin} from './src/services/googleSigninConfig.tsx';
 
-configureGoogleLogin();
 AppRegistry.registerComponent(appName, () => App);
--- src/screens/App.tsx
+++ src/screens/App.tsx
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import React, {useEffect} from 'react';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
@@ -5,9 +6,11 @@ import SplashScreen from 'react-native-splash-screen';
 
 import {persistStore} from 'redux-persist';
 import {PersistGate} from 'redux-persist/integration/react';
-import {SafeAreaView} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import rootReducer from '../redux';
 import Test from '../screens/Test';
+import {configureGoogleLogin} from '../services/googleSigninConfig';
 
 const store = createStore(rootReducer);
 const persistor = persistStore(store);
@@ -15,15 +18,19 @@ const persistor = persistStore(store);
 function App(): JSX.Element {
   useEffect(() => {
     SplashScreen.hide();
+    configureGoogleLogin();
   }, []);
+
+  const Stack = createStackNavigator();
 
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        {/* <InvitationTokenCheckMW /> */}
-        <SafeAreaView>
-          <Test />
-        </SafeAreaView>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Test" component={Test} />
+          </Stack.Navigator>
+        </NavigationContainer>
       </PersistGate>
     </Provider>
   );
