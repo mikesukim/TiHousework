acfcd33dca9e3b93c20599d3c2bc42fbbc65a6b5
mikesukim <mikesungunkim@gmail.com>
2021-07-08 22:33:10 -0500

    add responsive fontsize

--- src/screens/Test.tsx
+++ src/screens/Test.tsx
@@ -1,13 +1,14 @@
 import React, {useEffect, useState} from 'react';
 import {Text, View} from 'react-native';
 import Websocket from '../components/Websocket';
+import {Header1} from '../styles/StyledComps'
 
 function Test(): JSX.Element {
   return (
     <View>
       {/* <Text>src/screen 에 위치한 Test 컴포넌트에, 테스트하고싶은 컴포넌트를 추가해 테스트하세요</Text> */}
       {/* <Websocket /> */}
-      <Text> hello world </Text>
+      <Header1> hello world </Header1>
     </View>
   );
 }
