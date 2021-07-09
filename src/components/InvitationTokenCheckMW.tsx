a8e0b431c267b9f16b0c127f7d40bb27560135e0
Jiyun Noh <ianjy1127@gmail.com>
2021-07-09 12:41:33 -0500

    Add buildLink

--- src/components/InvitationTokenCheckMW.tsx
+++ src/components/InvitationTokenCheckMW.tsx
@@ -7,12 +7,30 @@ import SocialLogin from './SocialLogin';
 import ApiTestComp from './ApiTestComp';
 
 function InvitationTokenCheckMW(): JSX.Element {
-  const {onUpdateIsInvited} = useUser();
+  const {email, onUpdateEmail, onUpdateIsInvited} = useUser();
   const {token} = useAuth();
 
   useEffect(() => {
+    onUpdateEmail('myapp@gmail.com');
+    buildLink(email);
+    // test();
     isFromLink();
   }, []);
+
+  async function buildLink(sender) {
+    // create a dynamic link (prefix + deep link)
+    const link = await dynamicLinks().buildLink({
+      // create a deep link
+      link: `https://appname.com/?sender=${sender}`,
+      // prefix on FB console
+      domainUriPrefix: 'https://tihouse.page.link',
+    });
+    console.log('링크 새로 만들었어요');
+    console.log(link);
+    return link;
+  }
+
+  // function buildShortLink()
 
   // 앱이 꺼져있는 상태에서 처음 킬 때
   function isFromLink() {
@@ -20,11 +38,36 @@ function InvitationTokenCheckMW(): JSX.Element {
       .getInitialLink()
       .then(link => {
         onUpdateIsInvited(true);
-        Alert.alert('네 저 링크로 들어왔어요');
+        // Alert.alert('네 저 링크로 들어왔어요');
+        // link.url 이 정확히 뭔지 모르겠음
+        // console.log(link);
+        // console.log(link.url);
+        if (
+          link.url.includes(
+            `https://tihouse.page.link/?link=https://appname.com/?sender`,
+          )
+          // link.url ===
+          // `https://tihouse.page.link/?link=https://appname.com/?sender=${sender}`
+        ) {
+          // link.url에서 sender value 가져오기
+          const query = new URLSearchParams(link.url);
+          const sender = query.get('sender');
+          console.log(sender);
+          Alert.alert('This is a manual home screen');
+        }
+        // Handle dynamic link inside your own application
         // if (link.url === 'https://invertase.io/offer') {
         //   // ...set initial route as offers screen
         // }
       });
+  }
+
+  function test() {
+    const query = new URLSearchParams(
+      `https://tihouse.page.link/?link=https://appname.com/?sender=123`,
+    );
+    // const sender = query.get('sender');
+    console.log(query);
   }
 
   function hasToken() {
