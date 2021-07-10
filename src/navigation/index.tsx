2261f3994b2c2291875e7a1880f46a5b1394846b
mikesukim <mikesungunkim@gmail.com>
2021-07-10 03:39:19 -0500

    add dynamic link flow

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import Test from '../screens/Test.tsx';
+import InvitationScreenTemp from '../components/InvitationScreenTemp.tsx'
 
 function Navigation(): JSX.Element {
   const Stack = createStackNavigator();
+
   return (
-    <Stack.Navigator>
+    <Stack.Navigator initialRouteName="Test">
       <Stack.Screen name="Test" component={Test} />
+      <Stack.Screen name="Invitation" component={InvitationScreenTemp} />
     </Stack.Navigator>
   );
 }
