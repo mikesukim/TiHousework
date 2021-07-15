8a304d799eb024995847321756a255abe8b34529
jiyunnoh <ianjy1127@gmail.com>
2021-07-14 23:39:26 -0500

    Complete WelcomeScreen

--- src/screens/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen.tsx
@@ -1,35 +1,29 @@
 import React from 'react';
 import dynamicLinks from '@react-native-firebase/dynamic-links';
-import {StyleSheet, Alert, Button, SafeAreaView} from 'react-native';
+import {Alert, SafeAreaView} from 'react-native';
 import Clipboard from '@react-native-clipboard/clipboard';
 import useUser from '../hooks/useUser.tsx';
 import {
-  Container,
-  TopContainer,
-  BottomContainer,
-  Header1,
   Header2,
-  Header3,
-  SendBtn,
   Image1,
   LogoText,
   View1,
   View2,
   View3,
   View4,
+  CustomButton,
+  CustomButtonText,
 } from '../styled-components/StyledComps';
 import ResetRedux from '../components/ResetRedux';
-import SocialLogin from '../components/SocialLogin';
 
 function WelcomeScreen(): JSX.Element {
-  const {email, isInvited, inviterEmail, onUpdateRoomID, onUpdateIsInvited} = useUser();
-
-  const styles = StyleSheet.create({
-    text: {
-      textAlign: 'center',
-      fontSize: 21,
-    },
-  });
+  const {
+    email,
+    isInvited,
+    inviterEmail,
+    onUpdateRoomID,
+    onUpdateIsInvited,
+  } = useUser();
 
   async function buildLink(param: string, value: string) {
     const link = await dynamicLinks().buildLink({
@@ -45,32 +39,30 @@ function WelcomeScreen(): JSX.Element {
         <View1>
           <View3>
             <LogoText>환영합니다</LogoText>
-            <Header2>집안일을 같이 관리할 방을 만들고</Header2>
-            <Header2>가족들을 초대해보세요</Header2>
+            <Header2>{inviterEmail} 님이 초대하셨네요</Header2>
           </View3>
           <View2>
-            <Image1 source={require('../img/refer.png')} />
+            <Image1 source={require('../img/welcome.jpeg')} />
           </View2>
           <View4>
-            <SocialLogin />
+            <CustomButton
+              style={{shadowOffset: {width: 3, height: 5}}}
+              onPress={() => {
+                Alert.alert(`${inviterEmail} 의 방으로 입장`);
+              }}>
+              <CustomButtonText>가족의 방으로 들어갈래요</CustomButtonText>
+            </CustomButton>
+            <CustomButton
+              style={{shadowOffset: {width: 3, height: 5}}}
+              onPress={() => {
+                onUpdateIsInvited(false);
+              }}>
+              <CustomButtonText>아니요, 새로운 방을 만들래요</CustomButtonText>
+            </CustomButton>
+            <ResetRedux />
           </View4>
         </View1>
       </SafeAreaView>
-      // <Container>
-      //   <TopContainer>
-      //     <Header1>Title</Header1>
-      //   </TopContainer>
-      //   <BottomContainer>
-      //     <Header2>안녕하세요 티집살림이에요!</Header2>
-      //     <Header3>{inviterEmail} 님이 초대하셨네요!</Header3>
-      //     <SendBtn
-      //       title="방에 입장"
-      //       onPress={() => Alert.alert(`${inviterEmail} 의 방으로 입장`)}
-      //       bgColor="#e21a5f"
-      //     />
-      //     <ResetRedux />
-      //   </BottomContainer>
-      // </Container>
     );
   }
   return (
@@ -85,35 +77,28 @@ function WelcomeScreen(): JSX.Element {
           <Image1 source={require('../img/refer.png')} />
         </View2>
         <View4>
-          <SocialLogin />
+          <CustomButton
+            style={{shadowOffset: {width: 3, height: 5}}}
+            onPress={() => {
+              // room ID는 어떻게 만들어줄건지?
+              onUpdateRoomID('1');
+              Alert.alert('새로운 방이 만들어졌어요');
+            }}>
+            <CustomButtonText>우리 가족의 방을 만들래요</CustomButtonText>
+          </CustomButton>
+          <CustomButton
+            style={{shadowOffset: {width: 3, height: 5}}}
+            onPress={async () => {
+              const link = buildLink('sender', email);
+              Clipboard.setString(await link);
+              Alert.alert('링크가 복사되었어요. 가족들에게 공유해보세요');
+            }}>
+            <CustomButtonText>우리 가족을 초대할래요</CustomButtonText>
+          </CustomButton>
+          <ResetRedux />
         </View4>
       </View1>
     </SafeAreaView>
-    // <Container>
-    //   <TopContainer>
-    //     <Header1>Title</Header1>
-    //   </TopContainer>
-    //   <BottomContainer>
-    //     <Header2>안녕하세요 티집살림이에요!</Header2>
-    //     <Header3>우리의 집안 살림을 같이 티낼 방을 만들어볼래요?</Header3>
-    //     <Button
-    //       title="방만들기"
-    //       onPress={() => {
-    //         onUpdateRoomID('123');
-    //         Alert.alert('새로운 방 번호가 생성되었어요');
-    //       }}
-    //     />
-    //     <Button
-    //       title="초대 링크 복사하기"
-    //       onPress={async () => {
-    //         const link = buildLink('sender', email);
-    //         Clipboard.setString(await link);
-    //         Alert.alert('링크가 복사되었습니다');
-    //       }}
-    //     />
-    //     <ResetRedux />
-    //   </BottomContainer>
-    // </Container>
   );
 }
 
