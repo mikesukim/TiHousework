8549613c2abf9424a95c0f35ed0c6df189a701e4
jiyunnoh <ianjy1127@gmail.com>
2021-07-14 22:49:14 -0500

    Complete OopsErrorScreen

--- src/screens/OopsErrorScreen.tsx
+++ src/screens/OopsErrorScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import ResetRedux from '../components/ResetRedux';
 import {
--- src/screens/Test.tsx
+++ src/screens/Test.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import InvitationTokenCheckMW from '../components/InvitationTokenCheckMW';
-import OopsErrorScreen from './OopsErrorScreen';
 
 function Test(): JSX.Element {
   return (
     <>
       {/* <Text>src/screen 에 위치한 Test 컴포넌트에, 테스트하고싶은 컴포넌트를 추가해 테스트하세요</Text> */}
       <InvitationTokenCheckMW />
-      {/* <OopsErrorScreen /> */}
     </>
   );
 }
