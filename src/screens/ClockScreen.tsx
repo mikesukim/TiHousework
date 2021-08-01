8f78b200fe4d07c01c6ca0f70dc9fbb96430e6c2
jiyunnoh <ianjy1127@gmail.com>
2021-08-01 17:32:13 -0500

    Add Bottom Tab

--- src/screens/ClockScreen.tsx
+++ src/screens/ClockScreen.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import {View} from 'react-native';
+import SafeAreaView from 'react-native-safe-area-view';
 import Clock from '../components/Clock.tsx';
 import Hello from '../components/Hello.tsx';
+import styles from '../styles';
 
 function ClockScreen(): JSX.Element {
   return (
-    <View>
+    <SafeAreaView style={styles.safeAreaView} forceInset={{top: 'always'}}>
       <Hello name="Michaaaaaeellll" />
       <Clock />
-    </View>
+    </SafeAreaView>
   );
 }
 
--- src/screens/HelloScreen.tsx
+++ src/screens/HelloScreen.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import {View} from 'react-native';
+import SafeAreaView from 'react-native-safe-area-view';
 import Hello from '../components/Hello.tsx';
+import styles from '../styles';
 
 function HelloScreen(): JSX.Element {
   return (
-    <View>
+    <SafeAreaView style={styles.safeAreaView} forceInset={{top: 'always'}}>
       <Hello name="Michaaaaaeellll" />
       <Hello name="Michaaaaaeellll" />
       <Hello name="Michaaaaaeellll" />
       <Hello name="Michaaaaaeellll" />
       <Hello />
-    </View>
+    </SafeAreaView>
   );
 }
 
