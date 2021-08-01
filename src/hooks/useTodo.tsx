c790f381a62b16d5ec3822de06f7f70f26976376
jiyunnoh <ianjy1127@gmail.com>
2021-07-31 23:10:49 -0500

    Update useTodo

--- src/hooks/useTodo.tsx
+++ src/hooks/useTodo.tsx
@@ -1,27 +1,18 @@
 import {useSelector, useDispatch} from 'react-redux';
 import {useCallback} from 'react';
 import {RootState} from '../redux';
-import {
-  addTodoItem,
-  removeTodoItem,
-  updateIsTodoDone,
-  removeIsTodoDone,
-} from '../redux/todo.ts';
+import {addTodoItem, removeTodoItem} from '../redux/todo.ts';
 
 interface TodoProps {
   todoItem: Array<{id: number; title: string; name: string; done: boolean}>;
-  isTodoDone: boolean;
   onAddTodoItem: (
     todoItem: Array<{id: number; title: string; name: string; done: boolean}>,
   ) => void;
   onRemoveTodoItem: (id: number) => void;
-  onUpdateIsTodoDone: (isTodoDone: boolean) => void;
-  onRemoveIsTodoDone: () => void;
 }
 
 export default function useTodo(): TodoProps {
   const todoItem = useSelector((state: RootState) => state.todo.todoItem);
-  const isTodoDone = useSelector((state: RootState) => state.todo.isTodoDone);
 
   const dispatch = useDispatch();
 
@@ -35,20 +26,10 @@ export default function useTodo(): TodoProps {
     (id: number) => dispatch(removeTodoItem(id)),
     [dispatch],
   );
-  const onUpdateIsTodoDone = useCallback(
-    (isTodoDone: boolean) => dispatch(updateIsTodoDone(isTodoDone)),
-    [dispatch],
-  );
-  const onRemoveIsTodoDone = useCallback(() => dispatch(removeIsTodoDone()), [
-    dispatch,
-  ]);
 
   return {
     todoItem,
-    isTodoDone,
     onAddTodoItem,
     onRemoveTodoItem,
-    onUpdateIsTodoDone,
-    onRemoveIsTodoDone,
   };
 }
--- src/redux/todo.ts
+++ src/redux/todo.ts
@@ -1,8 +1,6 @@
 // Declare Action type for typescript
 const ADD_TODOITEM = 'todo/ADD_TODOITEM' as const;
 const REMOVE_TODOITEM = 'todo/REMOVE_TODOITEM' as const;
-const UPDATE_ISTODODONE = 'todo/UPDATE_ISTODODONE' as const;
-const REMOVE_ISTODODONE = 'todo/REMOVE_ISTODODONE' as const;
 
 // Declare Action type for Redux
 interface actionType {
@@ -20,31 +18,20 @@ export const removeTodoItem = (id: number): actionType => ({
   type: REMOVE_TODOITEM,
   payload: id,
 });
-export const updateIsTodoDone = (isTodoDone: boolean): actionType => ({
-  type: UPDATE_ISTODODONE,
-  payload: isTodoDone,
-});
-export const removeIsTodoDone = (): actionType => ({
-  type: REMOVE_ISTODODONE,
-});
 
 // Declare Reducers
 type TodoAction =
   | ReturnType<typeof addTodoItem>
-  | ReturnType<typeof removeTodoItem>
-  | ReturnType<typeof updateIsTodoDone>
-  | ReturnType<typeof removeIsTodoDone>;
+  | ReturnType<typeof removeTodoItem>;
 
 type TodoState = {
   todoItem: Array<{id: number; title: string; name: string; done: boolean}>;
-  isTodoDone: boolean;
 };
 
 const initialState: TodoState = {
   todoItem: [
     {id: Math.random(), title: '설거지 하기', name: '지윤', done: false},
   ],
-  isTodoDone: false,
 };
 
 function todo(state: TodoState = initialState, action: TodoAction): TodoState {
@@ -56,10 +43,6 @@ function todo(state: TodoState = initialState, action: TodoAction): TodoState {
         ...state,
         todoItem: state.todoItem.filter(item => item.id !== action.payload),
       };
-    case UPDATE_ISTODODONE:
-      return {...state, isTodoDone: action.payload};
-    case REMOVE_ISTODODONE:
-      return {...state, isTodoDone: false};
     default:
       return state;
   }
